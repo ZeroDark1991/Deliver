792768b13ac90c1b755e7960f559b9db707f88ee
shiweipeng <549834931@qq.com>
2017-01-06 23:17:33 +0800

    no message

--- user_app/src/App.vue
+++ user_app/src/App.vue
@@ -41,8 +41,8 @@ export default {
 	return {
 		mask:false,
 		show:false,
-		PhoneNumber: '13588277370',//13588277370
-		VerifyCode: '438811',//438811
+		PhoneNumber: '',//13588277370
+		VerifyCode: '',//438811
 		isGetCheck:false,
 		hint: '获取短信验证码',
 		timer: null,
